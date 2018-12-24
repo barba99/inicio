5392603a943a4470b5f1854658f034d885aae79d
barba99 <38580791+barba99@users.noreply.github.com>
2018-12-23 21:13:26 -0600

    Create Instalacion.sh

---
+++ Instalacion.sh
@@ -0,0 +1,19 @@
+#bin/bash
+b="\033[1;37m"
+v="\033[1;32m"
+Ver="\033[1;36m"
+m="\033[0;34m"
+barra="\033[0;34m∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞=∞\033[0m"
+
+
+echo -e  "$ver             BIENVENIDOS SCRIPT VPS-BARBA... "
+echo " "
+echo -e  "$v                                 -@Jorge_Barba "
+echo " "
+echo " "
+echo " "
+echo -e  "$ver                ESCRIBA SU NOMBRE.. :"
+read varbanner
+echo "toilet -f big '    $varbanner' -F gay | lolcat" >Homero.txt
+cat "Homero.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+
